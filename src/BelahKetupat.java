e7295123834c00d8add2de8ae1878fa805b8606a
Dimasghfri <dimasimamghifari@webmail.umm.ac.id>
2023-11-01 23:26:35 +0800

    Initial commit

---
+++ src/BelahKetupat.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class BelahKetupat {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan Panjang Sisi: ");
+        double sisi = input.nextDouble();
+        System.out.print("Masukkan Diagonal 1: ");
+        double D1 = input.nextDouble();
+        System.out.print("Masukkan Diagonal 2: ");
+        double D2 = input.nextDouble();
+
+         double keliling = 4 * sisi;
+          double luas = (D1 * D2)/0.5;
+        /**
+         * @param sisi Panjang sisi belah ketupat
+         * @return luas dari belah ketupat
+         */
+        System.out.println("Keliling dari Bangun Belah Ketupat adalah: "+keliling);
+
+        System.out.println("Luas dari Bangun Belah Ketupat adalah: "+luas);
+
+    }
+}
